b87a087da5fe78be33bb856fa935bc7b9543752a
QY <marvinyao@icloud.com>
2024-05-05 01:04:04 +0200

    Title Page

---
+++ info.js
@@ -0,0 +1,10 @@
+const works = {
+    "dacapo" : {
+        "title" : "Da Capo",
+        "subtitle" : "Music Player",
+        "description" : "A simple music player for Android.",
+        "image" : "dacapo.png",
+    },
+    
+
+}
